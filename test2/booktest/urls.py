6935fe4337704e8074826c3fa0c547d50f39e50d
HEKEH <hekai02@163.com>
2018-08-07 22:31:56 +0800

    2018/8/7

--- test2/booktest/urls.py
+++ test2/booktest/urls.py
@@ -4,4 +4,14 @@ from . import views
 urlpatterns =[
     path('', views.index),
     url('^(?P<p1>\d+)/(?P<p3>\d+)/(?P<p2>\d+)$', views.detail),
+    url('^gettest1$', views.getTest1),
+    url('^gettest2$', views.getTest2),
+    url('^gettest3$', views.getTest3),
+    url('^posttest1$', views.postTest1),
+    url('^posttest2$', views.postTest2),
+    url('^cookietest$', views.cookieTest),
+    url('^redtest1$', views.redTest1),
+    url('^redtest2$', views.redTest2),
+    url('^session1$', views.session1),
+    url('^session2$', views.session2),
 ]
--- test2/booktest/views.py
+++ test2/booktest/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.db.models import Max,F,Q
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 from .models import *
@@ -18,3 +18,63 @@ def index(request):
 def detail(request, p1, p2, p3):
     #return HttpResponse(request.GET)
     return HttpResponse('year:%s, month:%s, day:%s'%(p1, p2, p3))
+
+def getTest1(request):
+    return render(request, 'booktest/getTest1.html')
+
+def getTest2(request):
+    a = request.GET.get('a')
+    b = request.GET.get('b')
+    c = request.GET.get('c')
+    context = {
+        'a': a,
+        'b': b,
+        'c': c,
+    }
+    return render(request, 'booktest/getTest2.html',context)
+
+def getTest3(request):
+    list1 = request.GET.getlist('a')
+    context = {
+        'list': list1,
+    }
+    return render(request, 'booktest/getTest3.html',context)
+
+def postTest1(request):
+    return render(request, 'booktest/postTest1.html')
+
+def postTest2(request):
+    uname = request.POST.get('uname')
+    ugender = request.POST.get('ugender')
+    uhobby = request.POST.getlist('uhobby')
+    context = {
+        "uname": uname,
+        "ugender": ugender,
+        "uhobby": uhobby,
+    }
+    return render(request, 'booktest/postTest2.html', context)
+
+def cookieTest(request):
+    response = HttpResponse()
+    if 't1' in request.COOKIES:
+        response.write('<h1>' + request.COOKIES['t1'] + '</h1>')
+    else:
+        response.set_cookie('t1','abc')
+    return response
+
+def redTest1(request):
+    return HttpResponseRedirect('redtest2')
+    #or return redirect('redtest2')
+
+def redTest2(request):
+    return HttpResponse('转向')
+
+def session1(request):
+    uname = request.session.get('uname','新用户')
+    context = {'uname' : uname}
+    return render(request, 'booktest/session1.html', context)
+
+def session2(request):
+    uname = request.POST.get('uname')
+    request.session['uname'] = uname
+    return redirect('/booktest/session1')
